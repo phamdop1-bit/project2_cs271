01dd9e65ff0f57a310097e900995b36d512c0d54
RedNelson <137230162+RedHeadFred923@users.noreply.github.com>
2025-09-30 11:12:13 -0400

    test template

---
+++ test_minqueue.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "minqueue.cpp"
+#include "usecase.cpp"
+
+void test_minqueue(){
+
+}
+
+void test_insert(){
+
+}
+
+void test_min(){
+
+}
+
+void test_extract_min(){
+
+}
+
+void test_decrease_key(){
+
+}
+
+void test_min_heapify(){
+
+}
+
+void test_build_heap(){
+
+}
+
+void test_sort(){
+
+}
+
+void test_sliding_window(){
+
+}
+
+void test_to_string(){
+
+}
+
+void test_size(){
+
+}
+
+void test_empty(){
+
+}
+
+int main(){
+    test_minqueue();
+    test_insert();
+    test_min();
+    test_extract_min();
+    test_decrease_key();
+    test_min_heapify();
+    test_build_heap();
+    test_sort();
+    test_sliding_window();
+    test_to_string();
+    test_size();
+    test_empty();
+}
